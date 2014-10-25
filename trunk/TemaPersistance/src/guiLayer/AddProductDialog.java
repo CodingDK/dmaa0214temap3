79cb78f3fbcc0d3b332fb3ee7ba4325884879d43
nonames.dk@gmail.com <nonames.dk@gmail.com@0e6a8468-aa74-9908-8ab1-487f95a0f36b>
2014-10-25 07:07:46 +0000

    Same product can't have more partorders anymore

--- trunk/TemaPersistance/src/guiLayer/AddProductDialog.java
+++ trunk/TemaPersistance/src/guiLayer/AddProductDialog.java
@@ -1,15 +1,21 @@
 package guiLayer;
 
+import guiLayer.extensions.JDoubleField;
+import guiLayer.extensions.JTextFieldLimit;
+
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 import modelLayer.PartOrder;
 import modelLayer.Product;
@@ -19,26 +25,30 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 
+import ctrLayer.IFOrderCtr;
 import ctrLayer.OrderCtr;
 import ctrLayer.exceptions.NotEnoughStockException;
+import ctrLayer.exceptions.SubmitException;
 
 public class AddProductDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
-	private JTextField txtAmount;
 	private JTextField txtUnitPrice;
+	private JTextField txtAmount;
 	private Product prod;
 	private OrderPanel target;
 	private boolean done = false;
+	private PartOrder existingPO;
 
 	public AddProductDialog(Product prod, OrderPanel target) {
 		super((JDialog) null, true);
-		setResizable(false);
+		this.target = target;
 		this.prod = prod;
+		this.existingPO = getExistingPartOrder();
+		setResizable(false);
 		this.target = target;
 		this.setSize(new Dimension(270, 170));
 		this.setResizable(false);
-
-		setTitle("Add Product");
+				
 		getContentPane().setLayout(
 				new FormLayout(new ColumnSpec[] {
 						FormFactory.RELATED_GAP_COLSPEC,
@@ -62,26 +72,35 @@ public class AddProductDialog extends JDialog {
 								FormFactory.DEFAULT_ROWSPEC,
 								RowSpec.decode("13dlu"),
 								FormFactory.DEFAULT_ROWSPEC, }));
+		
+		ActionListener escListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		};
+		getRootPane().registerKeyboardAction(escListener,
+				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
 
 		JLabel lblTitle = new JLabel("#" + prod.getId() + " - "
 				+ prod.getName());
 		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		getContentPane().add(lblTitle, "5, 3, center, default");
 
-		JLabel lblPrice = new JLabel("Unit price:");
-		getContentPane().add(lblPrice, "3, 6, left, default");
-
-		txtUnitPrice = new JTextField("" + prod.getSalesPrice());
-		getContentPane().add(txtUnitPrice, "5, 6, 3, 1, fill, default");
-		txtUnitPrice.setColumns(10);
-
 		JLabel lblAmount = new JLabel("Amount:");
-		getContentPane().add(lblAmount, "3, 8, left, default");
+		getContentPane().add(lblAmount, "3, 6, left, default");
 
-		txtAmount = new JTextField();
-		getContentPane().add(txtAmount, "5, 8, 3, 1, fill, default");
-		txtAmount.setColumns(10);
+		txtAmount = new JTextField(10);
+		txtAmount.setDocument(new JTextFieldLimit(6, true, false));
+		getContentPane().add(txtAmount, "5, 6, 3, 1, fill, default");
 
+		JLabel lblUnitPrice = new JLabel("Unit price:");
+		getContentPane().add(lblUnitPrice, "3, 8, left, default");
+
+		txtUnitPrice = new JDoubleField(10);
+		getContentPane().add(txtUnitPrice, "5, 8, 3, 1, fill, default");
+		
 		JButton btnAdd = new JButton("Add");
 		btnAdd.addActionListener(new ActionListener() {
 			@Override
@@ -89,45 +108,71 @@ public class AddProductDialog extends JDialog {
 				addProduct();
 			}
 		});
-		// JRootPane rootPane = SwingUtilities.getRootPane(btnAdd);
-		// rootPane.setDefaultButton(btnAdd);
 		getContentPane().add(btnAdd, "5, 10");
-
+		
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				exit();
+				setVisible(false);
 			}
 		});
 		getContentPane().add(btnCancel, "7, 10, right, default");
+		
+		
+		if (existingPO == null) {
+			setTitle("Add Product");
+			txtUnitPrice.setText(Double.toString(prod.getSalesPrice()));
+			txtAmount.requestFocus();
+		} else {
+			setTitle("Edit PartOrder");
+			txtAmount.setText(Integer.toString(existingPO.getAmount()));
+			txtUnitPrice.setText(Double.toString(existingPO.getUnitPrice()));
+			btnAdd.setText("Edit");
+		}
+		this.getRootPane().setDefaultButton(btnAdd);
+	}
+
+	private PartOrder getExistingPartOrder() {
+		PartOrder retPO = null;
+		int i = 0;
+		boolean found = false;
+		ArrayList<PartOrder> partOrders = target.getPartOrders();
+		while (i < partOrders.size() && !found) {
+			if (partOrders.get(i).getProduct().getId() == prod.getId()) {
+				retPO = partOrders.get(i);
+				found = true;
+			}
+			i++;
+		}
+		return retPO;
 	}
 
 	private void addProduct() {
 		try {
-			OrderCtr oCtr = new OrderCtr(); // TODO IFOrderCtr -- Giver syntax:
-											// Unhandled error
+			IFOrderCtr oCtr = new OrderCtr(); 
+			
 			Product product = prod;
+			double unitPrice = Double.parseDouble(txtUnitPrice.getText());
 			int amount = Integer.parseInt(txtAmount.getText());
-			Double unitPrice = Double.parseDouble(txtUnitPrice.getText());
-			try {
-				PartOrder po = oCtr.createPartOrder(product, amount, unitPrice);
-				target.addProductToOrder(po);
-				done = true;
-				setVisible(false);
-			} catch (NotEnoughStockException e) {
-				JOptionPane.showMessageDialog(this, e.getMessage(), "Error",
-						JOptionPane.ERROR_MESSAGE);
+			if(amount <= 0) {
+				throw new SubmitException("Amount have to be greather than 0", txtAmount);
 			}
-		} catch (NumberFormatException e) {
-			JOptionPane.showMessageDialog(this,
-					"Amount must be a whole number", "Error",
-					JOptionPane.ERROR_MESSAGE);
+			PartOrder po = null;
+			if(existingPO == null) {
+				po = oCtr.createPartOrder(product, amount, unitPrice);	
+			} else {
+				po = oCtr.editPartOrder(existingPO, amount, unitPrice);
+			}
+			target.addPartOrder(po);
+			done = true;
+			setVisible(false);
+		
+		} catch (SubmitException e) {
+			e.showError();
+		} catch (NotEnoughStockException e) {
+			e.showDialog();
 		}
-	}
-
-	private void exit() {
-		setVisible(false);
 	}
 
 	public boolean isDone() {
